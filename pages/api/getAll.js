747ecb0b8d5735bb19fb5f88029d69a105ba8c58
Vishal <vishal.kondle@gmail.com>
2023-05-08 16:19:17 +0530

    error checking getAll

--- pages/api/getAll.js
+++ pages/api/getAll.js
@@ -27,37 +27,39 @@ const handler = async (req, res) => {
 
       // Total
       // Total Money Distributed
-      var loansSum = await loan.aggregate([ 
-        { $group: { _id: null, amount: { $sum: "$loanAmount" } } }
-      ])
+      var loansSum = await loan.aggregate([
+        { $group: { _id: null, amount: { $sum: "$loanAmount" } } },
+      ]);
       // Total Money Collected
-      var installmentsSum = await installment.aggregate([ 
-        { $group: { _id: null, amount: { $sum: "$installment" } } }
-      ])
+      var installmentsSum = await installment.aggregate([
+        { $group: { _id: null, amount: { $sum: "$installment" } } },
+      ]);
       // Total Interest Collected
-      var interestSum = await loan.aggregate([ 
-        { $group: { _id: null, amount: { $sum: "$interest" } } }
-      ])
+      var interestSum = await loan.aggregate([
+        { $group: { _id: null, amount: { $sum: "$interest" } } },
+      ]);
       // Total Fees Collected
-      var chargesSum = await loan.aggregate([ 
-        { $group: { _id: null, amount: { $sum: "$charges" } } }
-      ])
+      var chargesSum = await loan.aggregate([
+        { $group: { _id: null, amount: { $sum: "$charges" } } },
+      ]);
 
       // Counts
       // 10 Weeks NF Count
-      var tenWeeksNF = await loan.countDocuments({count: 10});
+      var tenWeeksNF = await loan.countDocuments({ count: 10 });
       // 100 Days NF Count
-      var hundredDaysNF = await loan.countDocuments({count: 100});
+      var hundredDaysNF = await loan.countDocuments({ count: 100 });
       // 52 Weeks BC Count
-      var fiftyTwoWeeksBC = await loan.countDocuments({count: 52});
+      var fiftyTwoWeeksBC = await loan.countDocuments({ count: 52 });
       // 20 Months BC Count
-      var twentyMonthsBC = await loan.countDocuments({count: 20});
+      var twentyMonthsBC = await loan.countDocuments({ count: 20 });
       // 17 Months BC Count
-      var seventeenMonthsBC = await loan.countDocuments({count: 17});
+      var seventeenMonthsBC = await loan.countDocuments({ count: 17 });
       // 12 Months BC Count
-      var twelveMonthsBC = await loan.countDocuments({count: 12});
+      var twelveMonthsBC = await loan.countDocuments({ count: 12 });
 
-      var totalBC = await loan.find({ $or: [{ count: 12 }, { count: 52 }, { count: 17 }, { count: 20 }] });
+      var totalBC = await loan.find({
+        $or: [{ count: 12 }, { count: 52 }, { count: 17 }, { count: 20 }],
+      });
       var totalNF = await loan.find({ $or: [{ count: 10 }, { count: 100 }] });
 
       return res.status(200).json({
@@ -65,116 +67,116 @@ const handler = async (req, res) => {
         ok: true,
         data: [
           {
-            "label": "Total Customers Count",
-            "stats": customers,
-            "progress": 100,
-            "color": "teal",
-            "icon": "users"
+            label: "Total Customers Count",
+            stats: customers,
+            progress: 100,
+            color: "teal",
+            icon: "users",
           },
           {
-            "label": "Total Loans Count",
-            "stats": loans,
-            "progress": 100,
-            "color": "blue",
-            "icon": "currency"
+            label: "Total Loans Count",
+            stats: loans,
+            progress: 100,
+            color: "blue",
+            icon: "currency",
           },
           {
-            "label": "Total Installments Count",
-            "stats": installments,
-            "progress": 100,
-            "color": "red",
-            "icon": "ruppee"
+            label: "Total Installments Count",
+            stats: installments,
+            progress: 100,
+            color: "red",
+            icon: "ruppee",
           },
           {
-            "label": "Total Groups Count",
-            "stats": groups,
-            "progress": 100,
-            "color": "teal",
-            "icon": "bank"
+            label: "Total Groups Count",
+            stats: groups,
+            progress: 100,
+            color: "teal",
+            icon: "bank",
           },
           {
-            "label": "Total Money Distributed",
-            "stats": loansSum[0].amount,
-            "progress": 100,
-            "color": "lime",
-            "icon": "currency"
+            label: "Total Money Distributed",
+            stats: loansSum[0].amount,
+            progress: 100,
+            color: "lime",
+            icon: "currency",
           },
           {
-            "label": "Total Money Collected",
-            "stats": installmentsSum[0].amount,
-            "progress": 100,
-            "color": "pink",
-            "icon": "ruppee"
+            label: "Total Money Collected",
+            stats: installmentsSum[0].amount,
+            progress: 100,
+            color: "pink",
+            icon: "ruppee",
           },
           {
-            "label": "Total Interest Collected",
-            "stats": interestSum[0].amount,
-            "progress": 100,
-            "color": "grape",
-            "icon": "ruppee"
+            label: "Total Interest Collected",
+            stats: interestSum[0].amount,
+            progress: 100,
+            color: "grape",
+            icon: "ruppee",
           },
           {
-            "label": "Total Fees Collected",
-            "stats": chargesSum[0].amount,
-            "progress": 100,
-            "color": "violet",
-            "icon": "ruppee"
+            label: "Total Fees Collected",
+            stats: chargesSum[0].amount,
+            progress: 100,
+            color: "violet",
+            icon: "ruppee",
           },
           {
-            "label": "10 Weeks N.F. Count",
-            "stats": tenWeeksNF,
-            "progress": 100,
-            "color": "green",
-            "icon": "number"
+            label: "10 Weeks N.F. Count",
+            stats: tenWeeksNF,
+            progress: 100,
+            color: "green",
+            icon: "number",
           },
           {
-            "label": "100 Days N.F. Count",
-            "stats": hundredDaysNF,
-            "progress": 100,
-            "color": "yellow",
-            "icon": "number"
+            label: "100 Days N.F. Count",
+            stats: hundredDaysNF,
+            progress: 100,
+            color: "yellow",
+            icon: "number",
           },
           {
-            "label": "17 Months N.F. Count",
-            "stats": seventeenMonthsBC,
-            "progress": 100,
-            "color": "green",
-            "icon": "number"
+            label: "17 Months N.F. Count",
+            stats: seventeenMonthsBC,
+            progress: 100,
+            color: "green",
+            icon: "number",
           },
           {
-            "label": "12 Months N.F. Count",
-            "stats": twelveMonthsBC,
-            "progress": 100,
-            "color": "orange",
-            "icon": "number"
+            label: "12 Months N.F. Count",
+            stats: twelveMonthsBC,
+            progress: 100,
+            color: "orange",
+            icon: "number",
           },
           {
-            "label": "52 Weeks B.C. Count",
-            "stats": twentyMonthsBC,
-            "progress": 100,
-            "color": "indigo",
-            "icon": "number"
+            label: "52 Weeks B.C. Count",
+            stats: twentyMonthsBC,
+            progress: 100,
+            color: "indigo",
+            icon: "number",
           },
           {
-            "label": "20 Month B.C. Count",
-            "stats": fiftyTwoWeeksBC,
-            "progress": 100,
-            "color": "cyan",
-            "icon": "number"
+            label: "20 Month B.C. Count",
+            stats: fiftyTwoWeeksBC,
+            progress: 100,
+            color: "cyan",
+            icon: "number",
           },
           {
-            "label": "Total B.C. Count",
-            "stats": totalBC.length,
-            "progress": 100,
-            "color": "dark",
-            "icon": "number"
+            label: "Total B.C. Count",
+            stats: totalBC.length,
+            progress: 100,
+            color: "dark",
+            icon: "number",
           },
           {
-            "label": "Total N.F. Count",
-            "stats": totalNF.length,
-            "progress": 100,
-            "color": "gray",
-            "icon": "number"
+            label: "Total N.F. Count",
+            stats: totalNF.length,
+            progress: 100,
+            color: "gray",
+            icon: "number",
           },
         ],
         message: "Customers fetched successfully",
@@ -183,7 +185,7 @@ const handler = async (req, res) => {
       return res.status(500).json({
         error: true,
         ok: false,
-        data: error,
+        data: error.message,
         message: "Erorr while fetching loans",
       });
     }
