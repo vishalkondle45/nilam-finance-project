5fb906ffe2699a041b2a0e3cc2a8f437005462f6
vishalkondle45 <vishal.kondle@gmail.com>
2023-07-05 20:21:05 +0530

    fines added

--- pages/api/getAll.js
+++ pages/api/getAll.js
@@ -66,131 +66,175 @@ const handler = async (req, res) => {
       });
       var totalNF = await loan.find({ $or: [{ count: 10 }, { count: 100 }] });
 
+      var NFFineSum = await installment.aggregate([
+        {
+          $lookup: {
+            from: "loans",
+            localField: "loan",
+            foreignField: "id",
+            as: "loanDetails",
+          },
+        },
+        {
+          $unwind: "$loanDetails",
+        },
+        {
+          $group: {
+            _id: "$loanDetails.count",
+            amount: { $sum: "$fine" },
+          },
+        },
+      ]);
+
+      const data = [
+        {
+          label: "Total Customers Count",
+          stats: customers,
+          progress: 100,
+          color: "teal",
+          icon: "users",
+        },
+        {
+          label: "Total Loans Count",
+          stats: loans,
+          progress: 100,
+          color: "blue",
+          icon: "currency",
+        },
+        {
+          label: "Total Installments Count",
+          stats: installments,
+          progress: 100,
+          color: "red",
+          icon: "ruppee",
+        },
+        {
+          label: "Total Groups Count",
+          stats: groups,
+          progress: 100,
+          color: "teal",
+          icon: "bank",
+        },
+        {
+          label: "Total Money Distributed",
+          stats: loansSum[0]?.amount || 0,
+          progress: 100,
+          color: "lime",
+          icon: "currency",
+        },
+        {
+          label: "Total Money Collected",
+          stats: installmentsSum[0]?.amount || 0,
+          progress: 100,
+          color: "pink",
+          icon: "ruppee",
+        },
+        {
+          label: "Total Interest Collected",
+          stats: NFInterestSum[0]?.amount || 0,
+          progress: 100,
+          color: "grape",
+          icon: "ruppee",
+        },
+        {
+          label: "Total Fees Collected",
+          stats: chargesSum[0]?.amount || 0,
+          progress: 100,
+          color: "violet",
+          icon: "ruppee",
+        },
+        {
+          label: "10 Weeks N.F. Count",
+          stats: tenWeeksNF,
+          progress: 100,
+          color: "green",
+          icon: "number",
+        },
+        {
+          label: "100 Days N.F. Count",
+          stats: hundredDaysNF,
+          progress: 100,
+          color: "yellow",
+          icon: "number",
+        },
+        {
+          label: "17 Months N.F. Count",
+          stats: seventeenMonthsBC,
+          progress: 100,
+          color: "green",
+          icon: "number",
+        },
+        {
+          label: "12 Months N.F. Count",
+          stats: twelveMonthsBC,
+          progress: 100,
+          color: "orange",
+          icon: "number",
+        },
+        {
+          label: "52 Weeks B.C. Count",
+          stats: fiftyTwoWeeksBC,
+          progress: 100,
+          color: "indigo",
+          icon: "number",
+        },
+        {
+          label: "20 Month B.C. Count",
+          stats: twentyMonthsBC,
+          progress: 100,
+          color: "cyan",
+          icon: "number",
+        },
+        {
+          label: "Total B.C. Count",
+          stats: totalBC?.length,
+          progress: 100,
+          color: "dark",
+          icon: "number",
+        },
+        {
+          label: "Total N.F. Count",
+          stats: totalNF?.length,
+          progress: 100,
+          color: "gray",
+          icon: "number",
+        },
+        {
+          label: "Total B.C. Interest",
+          stats: BCInterestSum[0]?.amount || 0,
+          progress: 100,
+          color: "pink",
+          icon: "number",
+        },
+      ];
+
+      const colors = [
+        "green",
+        "violet",
+        "pink",
+        "teal",
+        "yellow",
+        "red",
+        "blue",
+        "orange",
+      ];
+
+      const fine = NFFineSum.map((item, index) => {
+        return {
+          label: item._id + " Installments - Fine",
+          stats: item.amount,
+          progress: 100,
+          color: colors[index],
+          icon: "currency",
+        };
+      });
+
       return res.status(200).json({
         error: false,
         ok: true,
-        data: [
-          {
-            label: "Total Customers Count",
-            stats: customers,
-            progress: 100,
-            color: "teal",
-            icon: "users",
-          },
-          {
-            label: "Total Loans Count",
-            stats: loans,
-            progress: 100,
-            color: "blue",
-            icon: "currency",
-          },
-          {
-            label: "Total Installments Count",
-            stats: installments,
-            progress: 100,
-            color: "red",
-            icon: "ruppee",
-          },
-          {
-            label: "Total Groups Count",
-            stats: groups,
-            progress: 100,
-            color: "teal",
-            icon: "bank",
-          },
-          {
-            label: "Total Money Distributed",
-            stats: loansSum[0]?.amount || 0,
-            progress: 100,
-            color: "lime",
-            icon: "currency",
-          },
-          {
-            label: "Total Money Collected",
-            stats: installmentsSum[0]?.amount || 0,
-            progress: 100,
-            color: "pink",
-            icon: "ruppee",
-          },
-          {
-            label: "Total Interest Collected",
-            stats: NFInterestSum[0]?.amount || 0,
-            progress: 100,
-            color: "grape",
-            icon: "ruppee",
-          },
-          {
-            label: "Total Fees Collected",
-            stats: chargesSum[0]?.amount || 0,
-            progress: 100,
-            color: "violet",
-            icon: "ruppee",
-          },
-          {
-            label: "10 Weeks N.F. Count",
-            stats: tenWeeksNF,
-            progress: 100,
-            color: "green",
-            icon: "number",
-          },
-          {
-            label: "100 Days N.F. Count",
-            stats: hundredDaysNF,
-            progress: 100,
-            color: "yellow",
-            icon: "number",
-          },
-          {
-            label: "17 Months N.F. Count",
-            stats: seventeenMonthsBC,
-            progress: 100,
-            color: "green",
-            icon: "number",
-          },
-          {
-            label: "12 Months N.F. Count",
-            stats: twelveMonthsBC,
-            progress: 100,
-            color: "orange",
-            icon: "number",
-          },
-          {
-            label: "52 Weeks B.C. Count",
-            stats: fiftyTwoWeeksBC,
-            progress: 100,
-            color: "indigo",
-            icon: "number",
-          },
-          {
-            label: "20 Month B.C. Count",
-            stats: twentyMonthsBC,
-            progress: 100,
-            color: "cyan",
-            icon: "number",
-          },
-          {
-            label: "Total B.C. Count",
-            stats: totalBC?.length,
-            progress: 100,
-            color: "dark",
-            icon: "number",
-          },
-          {
-            label: "Total N.F. Count",
-            stats: totalNF?.length,
-            progress: 100,
-            color: "gray",
-            icon: "number",
-          },
-          {
-            label: "Total B.C. Interest",
-            stats: BCInterestSum[0]?.amount || 0,
-            progress: 100,
-            color: "pink",
-            icon: "number",
-          },
-        ],
+        data: [...data, ...fine],
         message: "Customers fetched successfully",
+        // NFFineSum,
       });
     } catch (error) {
       return res.status(500).json({
